2b5ac0c2474dc5406e2a9e9a5c1d448e1a15d2f0
annaVVV <vergeles.anna@gmail.com>
2017-08-14 22:20:39 -0700

    Add files via upload

---
+++ lib/data_strings.py
@@ -0,0 +1,19 @@
+
+SCRIPT_HEADER = """
+******************************************************************
+*                                                                *
+*               Books Search Application                         *
+*                                                                *
+******************************************************************
+
+"""
+TEMP_CSV = 'temp\\temp.csv'
+sort_str = """         1 - price
+     2 - avg ratings
+     3 - rating count
+     4 - published date
+     5 - page count
+     q - quit\n
+"""
+SORT_KEY = {'1':'price', '2': 'avg rating', '3': 'rating count', '4':'published date', '5': 'page count'}
+csv_file = TEMP_CSV
