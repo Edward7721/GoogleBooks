ffebf4d96d160d76b9ab77a768d442f29c4863e4
annaVVV <vergeles.anna@gmail.com>
2017-08-14 22:12:54 -0700

    Update getBooksJson.py

--- lib/getBooksJson.py
+++ lib/getBooksJson.py
@@ -10,8 +10,28 @@ class Api(object):
     def get(self, q = None,  maxResults = None):
         search_payload = {'q':q, 'maxResults':maxResults}
         resp = requests.get(self.base_url,params = search_payload)
-        print 'Response Status Code:', resp.status_code
+        status = resp.status_code
+        print 'Response Status Code:', status
+        if  status== 200:
+            return resp.text, status
+        return resp, status
 
-        if resp.status_code == 200:
-            return resp.text
-        return resp
+def nice_print_out( r):
+    header_string = ''
+    for key in r.request.headers:
+        header_string += '-H "%s: %s" ' % (key, r.request.headers[key])
+    if r.request.body:
+        print('Curl is:\n curl %s "%s" -d \'%s\' -X %s' % (header_string, r.request.url, r.request.body,
+                                                           r.request.method))
+    else:
+        print('Curl is:\n curl %s "%s" -X %s' % (header_string, r.request.url, r.request.method))
+    print('\nResponse status code: %s' % r.status_code)
+    if r.json != 'NoJSON':
+        try:
+            print('\nResponse body:\n %s' % json.dumps(r.json, indent=2))
+            print"============================================"
+            print json.dumps(r.json)
+        except TypeError:
+            print('\nResponse body:\n %s' % r.json)
+    else:
+        print('\nNo body in the response.')
